cec56a3595bfa70ee1889cde28ace83344de1ecb
timex <darkchinte2@gmail.com>
2022-08-09 02:58:20 +0800

    Add button that sets the desired pixels

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-const div = document.createElement('div');
-const container = document.querySelector('.container');
-div.classList.add('tile')
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', (e) => {
+    console.log(e);
+    let pixel = prompt("set pixel size:");
+    createTile(pixel);
+})
 
-for (let i = 0; i < (16 * 16); i++) {
-    container.appendChild(div.cloneNode(true))
+function createTile (n) {
+    if (n > 100) return alert("max pixel is 100");
+
+    const div = document.createElement('div');
+    const container = document.querySelector('.container');
+    div.classList.add('tile')
+    container.setAttribute('style', `grid-template:repeat(${n}, 1fr)/ repeat(${n}, 1fr);`)
+
+    for (let i = 0; i < (n * n); i++) {
+        container.appendChild(div.cloneNode(true))
+    }
+    const tiles = document.querySelectorAll('.tile');
+    tiles.forEach(tile => tile.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = 'black';
+    }))
 }
-
-const tiles = document.querySelectorAll('.tile');
-tiles.forEach(tile => tile.addEventListener('mouseover', (e) => {
-    e.target.style.backgroundColor = 'black';
-}))
