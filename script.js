3e1840ba8c5fc7345d809112e3e1dd5eaf55b39a
timex <darkchinte2@gmail.com>
2022-08-10 23:31:41 +0800

    Add rainbow feature

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const btn = document.querySelector('[data-btn="set"]');
 const reset = document.querySelector('[data-btn="reset"]');
+const rainbow = document.querySelector('[data-btn="rainbow"');
 const div = document.createElement('div');
 const container = document.querySelector('.container');
 
 div.classList.add('tile')
 
 btn.addEventListener('click', (e) => {
-    console.log(e);
     let pixel = prompt("set pixel size:");
     createTile(pixel);
 })
@@ -14,9 +14,11 @@ btn.addEventListener('click', (e) => {
 reset.addEventListener('click', () => {
     const tiles = document.querySelectorAll('.tile');
     tiles.forEach(tile => tile.style.backgroundColor = 'white');
+    tiles.forEach(tile => tile.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = 'black';
+    }));
+
 })
-
-
 
 function createTile(n) {
     if (n > 100) {
@@ -24,16 +26,29 @@ function createTile(n) {
     } else if (n == null) {
         return;
     }
-
     container.setAttribute('style', `grid-template:repeat(${n}, 1fr)/ repeat(${n}, 1fr);`)
-
     for (let i = 0; i < (n * n); i++) {
         container.appendChild(div.cloneNode(true))
     }
     const tiles = document.querySelectorAll('.tile');
     tiles.forEach(tile => tile.addEventListener('mouseover', (e) => {
         e.target.style.backgroundColor = 'black';
-    }))
+    }));
+
 }
+
+function random (max) {
+    return Math.floor(Math.random() * max);
+}
+
+
+rainbow.addEventListener('click', () => {
+    const tiles = document.querySelectorAll('.tile');
+    tiles.forEach(tile => tile.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = `hsl(${random(360)}, ${random(100)}%, ${random(100)}%)`;
+    }))
+})
+
+
 
 
